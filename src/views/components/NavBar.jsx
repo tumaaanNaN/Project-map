364a55f864114e7f3f0379b99ddc53bc7c0de62d
MsSmile23 <marko1357@yandex.ru>
2023-09-12 14:22:14 +0300

    Layout

--- src/views/components/NavBar.jsx
+++ src/views/components/NavBar.jsx
@@ -3,38 +3,51 @@ const React = require('react');
 function NavBar({ user }) {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
+      <nav className="navbar">
+      <div className='rege-buttons'>
+        <a href="/">
+          <button className="logo-brand">
+          Место для логотипа
+          </button>
+        </a>
         {user ? (
           <>
+            <p>{`Привет ${user.name}`}</p>
             <a href="/" className="navbar-brand">
               {user.login}
             </a>
             {' '}
-            <a href="/login" className="navbar-brand">
-              logout
+            <a href="/profile">
+              <button className="navbar-brand">
+                Профиль
+              </button>
             </a>
             {' '}
-            <a href="/register" className="navbar-brand">
-              global
+            <a href="/logout">
+              <button className="navbar-brand">
+                Выйти
+              </button>
             </a>
             {' '}
           </>
         ) : (
           <>
-            <a href="/" className="navbar-brand">
-              home
+            {' '}
+            <a href="/login">
+              <button className="navbar-brand">
+                Авторизоваться
+              </button>
             </a>
             {' '}
-            <a href="/login" className="navbar-brand">
-              login
-            </a>
-            {' '}
-            <a href="/register" className="navbar-brand">
-              register
+            <a href="/register">
+              <button className="navbar-brand">
+                Зарегистрироваться
+              </button>
             </a>
             {' '}
           </>
         )}
+        </div>
       </nav>
     </header>
   );
