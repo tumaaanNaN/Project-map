f0640565b17fb8a14c2b42f0ea6785c82159307a
Valeria_Zhdanova <val_1998@mail.ru>
2023-09-14 13:52:56 +0300

    profile design

--- src/views/Profile.jsx
+++ src/views/Profile.jsx
@@ -1,24 +1,59 @@
 const React = require('react');
 const Layout = require('./Layout');
+const { Typography, Grid, TextField, Container  } = require('@mui/material');
 
 module.exports = function Profile({ user }) {
   return (
     <Layout user={user}>
-      <h1>Профиль Пользователя</h1>
-      <form id="form">
-        <ul className="play">
-          <h2>Пользователь:</h2>
-          <li id="user">{user.login}</li>
-        </ul>
-        {/* <button id="prof" className="btn btn-primary"> Редактировать </button> */}
-      </form>
-      <div id="map" style={{ width: '500px', height: '370px' }} />
-      <input id="coordinates" type="text" name="coordinates" placeholder="Название маршрута" />
-      <br />
-      <input id="feedback" type="text" name="feedback" placeholder="Ваш отзыв" />
+      <Grid
+  container
+  direction="column"
+  alignItems="center"
+  justify="center"
+  marginTop='8px'
+  marginBottom="8px"
+  id="form" 
+  className="play"
+>
+      <Typography variant='h5' >
+      Профиль {user.login}
+      </Typography>
+      </Grid>
+      <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      sx={{ minHeight: '100vh' }}>
+      <div id="map" style={{ width: '700px', height: '500px' }} />
+      <Typography>
+        Введите название маршрута
+      </Typography>
+      <TextField 
+      variant="outlined" 
+    id="coordinates" 
+    type="text" 
+    name="coordinates" 
+    placeholder="Название маршрута"
+    margin="normal">
+      </TextField>
+      <Typography>
+        Введите населенный пункт
+      </Typography>
+      <TextField 
+      variant="outlined" 
+    id="city" 
+    type="text" 
+    name="cityName" 
+    placeholder="Название маршрута"
+    margin="normal">
+      </TextField>
+      {/* <input id="feedback" type="text" name="feedback" placeholder="Ваш отзыв" /> */}
       <script src="https://api-maps.yandex.ru/2.1/?apikey=454e7c2a-3167-48eb-bcbe-fd1c343cac62&lang=ru_RU" type="text/javascript" />
       <script src="https://yandex.st/jquery/2.2.3/jquery.min.js" type="text/javascript" />
       <script src="js/coordinates.js" />
+      </Grid>
     </Layout>
   );
 };
